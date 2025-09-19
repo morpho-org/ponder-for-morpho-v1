3ff4bf34f16eb978223f1dd4681450e2f95207c1
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2025-09-19 16:31:26 +0200

    fix:comment polygon

--- apps/ponder/ponder.config.ts
+++ apps/ponder/ponder.config.ts
@@ -13,7 +13,7 @@ export default createConfig({
     mainnet: { id: 1, rpc: process.env.PONDER_RPC_URL_1 },
     base: { id: 8453, rpc: process.env.PONDER_RPC_URL_8453 },
     unichain: { id: 130, rpc: process.env.PONDER_RPC_URL_130 },
-    polygon: { id: 137, rpc: process.env.PONDER_RPC_URL_137 },
+    // polygon: { id: 137, rpc: process.env.PONDER_RPC_URL_137 },
     katana: { id: 747474, rpc: process.env.PONDER_RPC_URL_747474 },
     arbitrum: { id: 42161, rpc: process.env.PONDER_RPC_URL_42161 },
     tac: { id: 239, rpc: process.env.PONDER_RPC_URL_239 },
@@ -54,10 +54,10 @@ export default createConfig({
           address: "0x8f5ae9CddB9f68de460C77730b018Ae7E04a140A",
           startBlock: 9139027,
         },
-        polygon: {
+        /* polygon: {
           address: "0x1bF0c2541F820E775182832f06c0B7Fc27A25f67",
           startBlock: 66931042,
-        },
+        }, */
         katana: {
           address: "0xD50F2DffFd62f94Ee4AEd9ca05C61d0753268aBc",
           startBlock: 2741069,
@@ -174,10 +174,10 @@ export default createConfig({
           address: "0xe9EdE3929F43a7062a007C3e8652e4ACa610Bdc0",
           startBlock: 9316789,
         },
-        polygon: {
+        /* polygon: {
           address: "0xa9c87daB340631C34BB738625C70499e29ddDC98",
           startBlock: 66931118,
-        },
+        }, */
         katana: {
           address: "0x1c8De6889acee12257899BFeAa2b7e534de32E16",
           startBlock: 2741420,
@@ -306,14 +306,14 @@ export default createConfig({
           }),
           startBlock: 9316789,
         },
-        polygon: {
+        /* polygon: {
           address: factory({
             address: "0xa9c87daB340631C34BB738625C70499e29ddDC98",
             event: getAbiItem({ abi: metaMorphoFactoryAbi, name: "CreateMetaMorpho" }),
             parameter: "metaMorpho",
           }),
           startBlock: 66931118,
-        },
+        }, */
         katana: {
           address: factory({
             address: "0x1c8De6889acee12257899BFeAa2b7e534de32E16",
@@ -516,10 +516,10 @@ export default createConfig({
           address: "0x9a6061d51743B31D2c3Be75D83781Fa423f53F0E",
           startBlock: 9139027,
         },
-        polygon: {
+        /* polygon: {
           address: "0xe675A2161D4a6E2de2eeD70ac98EEBf257FBF0B0",
           startBlock: 66931042,
-        },
+        }, */
         katana: {
           address: "0x4F708C0ae7deD3d74736594C2109C2E3c065B428",
           startBlock: 2741069,
@@ -630,10 +630,10 @@ export default createConfig({
           address: "0xb04e4D3D59Ee47Ca9BA192707AF13A7D02969911",
           startBlock: 9381237,
         },
-        polygon: {
+        /* polygon: {
           address: "0xeDadDe37D76c72b98725614d0b41C20Fe612d304",
           startBlock: 68074185,
-        },
+        }, */
         katana: {
           address: "0x678EB53A3bB79111263f47B84989d16D81c36D85",
           startBlock: 2741993,
