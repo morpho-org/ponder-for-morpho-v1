205134bba8c19a060ae04940d27fa565602e0d53
Hayden Shively <hayden@morpho.xyz>
2025-10-13 23:50:28 -0400

    wip3

--- apps/ponder/src/AdaptiveCurveIrm.ts
+++ apps/ponder/src/AdaptiveCurveIrm.ts
@@ -2,8 +2,14 @@ import { ponder } from "ponder:registry";
 import { market } from "ponder:schema";
 
 ponder.on("AdaptiveCurveIRM:BorrowRateUpdate", async ({ event, context }) => {
-  // Row must exist because `BorrowRateUpdate` cannot preceed `CreateMarket`.
-  await context.db
-    .update(market, { chainId: context.chain.id, id: event.args.id })
-    .set({ rateAtTarget: event.args.rateAtTarget });
+  try {
+    // Row must exist because `BorrowRateUpdate` cannot preceed `CreateMarket`.
+    await context.db
+      .update(market, { chainId: context.chain.id, id: event.args.id })
+      .set({ rateAtTarget: event.args.rateAtTarget });
+  } catch {
+    console.error(
+      `[${context.chain.name} (${context.chain.id.toString()})]: Tried to handle AdaptiveCurveIRM:BorrowRateUpdate before Morpho:CreateMarket completed. This indicates a bug in Ponder.`,
+    );
+  }
 });
